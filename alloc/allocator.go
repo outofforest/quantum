9921a109c53638b38eb4ad71aaa181aced38b1a3
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2024-10-11 16:03:02 +0200

    Benchmark token transfer transactions (#41)

--- alloc/allocator.go
+++ alloc/allocator.go
@@ -1,10 +1,78 @@
 package alloc
 
 import (
+	"github.com/pkg/errors"
+
 	"github.com/outofforest/quantum/list"
 	"github.com/outofforest/quantum/space"
 	"github.com/outofforest/quantum/types"
 )
+
+// Config stores configuration of allocator.
+type Config struct {
+	TotalSize uint64
+	NodeSize  uint64
+}
+
+// NewAllocator creates memory allocator.
+func NewAllocator(config Config) *Allocator {
+	numOfNodes := config.TotalSize / config.NodeSize
+	data := make([]byte, config.TotalSize)
+	availableNodes := map[types.NodeAddress][]byte{}
+	for i := types.NodeAddress(0x01); i < types.NodeAddress(numOfNodes); i++ {
+		availableNodes[i] = data[uint64(i)*config.NodeSize : uint64(i+1)*config.NodeSize]
+	}
+
+	return &Allocator{
+		config:         config,
+		data:           data,
+		availableNodes: availableNodes,
+	}
+}
+
+// Allocator is the allocator implementation used in tests.
+type Allocator struct {
+	config         Config
+	data           []byte
+	availableNodes map[types.NodeAddress][]byte
+}
+
+// Node returns node bytes.
+func (a *Allocator) Node(nodeAddress types.NodeAddress) []byte {
+	return a.data[uint64(nodeAddress)*a.config.NodeSize : uint64(nodeAddress+1)*a.config.NodeSize]
+}
+
+// Allocate allocates node and copies data into it.
+func (a *Allocator) Allocate(copyFrom []byte) (types.NodeAddress, []byte, error) {
+	var nodeAddress types.NodeAddress
+	var nodeData []byte
+	for nodeAddress, nodeData = range a.availableNodes {
+		break
+	}
+	if nodeData == nil {
+		return 0, nil, errors.New("out of space")
+	}
+
+	delete(a.availableNodes, nodeAddress)
+
+	if copyFrom == nil {
+		clear(nodeData)
+	} else {
+		copy(nodeData, copyFrom)
+	}
+
+	return nodeAddress, nodeData, nil
+}
+
+// Deallocate deallocates node.
+func (a *Allocator) Deallocate(nodeAddress types.NodeAddress) {
+	a.availableNodes[nodeAddress] = a.data[uint64(nodeAddress)*a.config.NodeSize : uint64(nodeAddress+1)*a.config.NodeSize]
+}
+
+// NodeSize returns size of node.
+func (a *Allocator) NodeSize() uint64 {
+	return a.config.NodeSize
+}
 
 // NewSnapshotAllocator returns snapshot-level allocator.
 func NewSnapshotAllocator(
@@ -66,7 +134,6 @@ func (sa SnapshotAllocator) Deallocate(nodeAddress types.NodeAddress, srcSnapsho
 	}
 
 	if _, exists := sa.snapshots.Get(srcSnapshotID); !exists {
-		delete(sa.dirtyNodes, nodeAddress)
 		sa.allocator.Deallocate(nodeAddress)
 		return nil
 	}
