c9ab9b34c612184f10fc54f099fea84675aa88ba
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2024-11-03 16:40:00 +0100

    Transpose chunks using pointers (#121)

--- checksum/benchmark_test.go
+++ checksum/benchmark_test.go
@@ -325,9 +325,14 @@ func BenchmarkTransposeAVX(b *testing.B) {
 	x := matrix
 	var z [16][16]uint32
 
+	xP := &[16]*[16]uint32{
+		&x[0], &x[1], &x[2], &x[3], &x[4], &x[5], &x[6], &x[7],
+		&x[8], &x[9], &x[10], &x[11], &x[12], &x[13], &x[14], &x[15],
+	}
+
 	b.StartTimer()
 	for range b.N {
-		Transpose(&x[0][0], &z[0][0])
+		Transpose(xP, &z)
 	}
 	b.StopTimer()
 
