ff588c424e546cc7f9845bc0bba84e0c19e73cfe
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-11-04 13:38:16 +0100

    Blake3 AVX512 implementation

--- checksum/blake3_test.go
+++ checksum/blake3_test.go
@@ -1,8 +1,9 @@
-package checksum_test
+package checksum
 
 import (
 	"crypto/rand"
 	"testing"
+	"unsafe"
 
 	blake3zeebo "github.com/zeebo/blake3"
 	blake3luke "lukechampine.com/blake3"
@@ -17,7 +18,14 @@ func randData(size uint) []byte {
 }
 
 var (
-	data1K = randData(1024)
+	data1K = func() [16][]byte {
+		return [16][]byte{
+			randData(1024), randData(1024), randData(1024), randData(1024),
+			randData(1024), randData(1024), randData(1024), randData(1024),
+			randData(1024), randData(1024), randData(1024), randData(1024),
+			randData(1024), randData(1024), randData(1024), randData(1024),
+		}
+	}()
 	data4K = randData(4096)
 )
 
@@ -53,30 +61,76 @@ func BenchmarkChecksum4KLuke(b *testing.B) {
 
 func BenchmarkChecksum1KZeebo(b *testing.B) {
 	for range b.N {
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
-		blake3zeebo.Sum256(data1K)
+		blake3zeebo.Sum256(data1K[0])
+		blake3zeebo.Sum256(data1K[1])
+		blake3zeebo.Sum256(data1K[2])
+		blake3zeebo.Sum256(data1K[3])
+		blake3zeebo.Sum256(data1K[4])
+		blake3zeebo.Sum256(data1K[5])
+		blake3zeebo.Sum256(data1K[6])
+		blake3zeebo.Sum256(data1K[7])
+		blake3zeebo.Sum256(data1K[8])
+		blake3zeebo.Sum256(data1K[9])
+		blake3zeebo.Sum256(data1K[10])
+		blake3zeebo.Sum256(data1K[11])
+		blake3zeebo.Sum256(data1K[12])
+		blake3zeebo.Sum256(data1K[13])
+		blake3zeebo.Sum256(data1K[14])
+		blake3zeebo.Sum256(data1K[15])
 	}
 }
 
 func BenchmarkChecksum1KLuke(b *testing.B) {
 	for range b.N {
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
-		blake3luke.Sum256(data1K)
+		blake3luke.Sum256(data1K[0])
+		blake3luke.Sum256(data1K[1])
+		blake3luke.Sum256(data1K[2])
+		blake3luke.Sum256(data1K[3])
+		blake3luke.Sum256(data1K[4])
+		blake3luke.Sum256(data1K[5])
+		blake3luke.Sum256(data1K[6])
+		blake3luke.Sum256(data1K[7])
+		blake3luke.Sum256(data1K[8])
+		blake3luke.Sum256(data1K[9])
+		blake3luke.Sum256(data1K[10])
+		blake3luke.Sum256(data1K[11])
+		blake3luke.Sum256(data1K[12])
+		blake3luke.Sum256(data1K[13])
+		blake3luke.Sum256(data1K[14])
+		blake3luke.Sum256(data1K[15])
 	}
+}
+
+func BenchmarkChecksum1KAVX(b *testing.B) {
+	b.StopTimer()
+	b.ResetTimer()
+
+	chunks := chunks
+
+	ch := [16]*byte{
+		(*byte)(unsafe.Pointer(&chunks[0][0])),
+		(*byte)(unsafe.Pointer(&chunks[1][0])),
+		(*byte)(unsafe.Pointer(&chunks[2][0])),
+		(*byte)(unsafe.Pointer(&chunks[3][0])),
+		(*byte)(unsafe.Pointer(&chunks[4][0])),
+		(*byte)(unsafe.Pointer(&chunks[5][0])),
+		(*byte)(unsafe.Pointer(&chunks[6][0])),
+		(*byte)(unsafe.Pointer(&chunks[7][0])),
+		(*byte)(unsafe.Pointer(&chunks[8][0])),
+		(*byte)(unsafe.Pointer(&chunks[9][0])),
+		(*byte)(unsafe.Pointer(&chunks[10][0])),
+		(*byte)(unsafe.Pointer(&chunks[11][0])),
+		(*byte)(unsafe.Pointer(&chunks[12][0])),
+		(*byte)(unsafe.Pointer(&chunks[13][0])),
+		(*byte)(unsafe.Pointer(&chunks[14][0])),
+		(*byte)(unsafe.Pointer(&chunks[15][0])),
+	}
+
+	var z [8][2][32]byte
+
+	b.StartTimer()
+	for range b.N {
+		Blake3(&ch[0], &z[0][0][0])
+	}
+	b.StopTimer()
 }
