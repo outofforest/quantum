332177c67fe7115085f34cf483a448a48ff4e23e
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-11-19 20:16:22 +0100

    Compare last item

--- space/compare/asm_test.go
+++ space/compare/asm_test.go
@@ -13,16 +13,16 @@ func TestCompare(t *testing.T) {
 	values := values
 
 	res := resDef
-	zeroIndex, count := Compare(0, &values[0], &res[0], 22)
+	zeroIndex, count := Compare(0, &values[0], &res[0], 23)
 	assert.Equal(t, [32]uint64{
-		5, 6, 13, 14, 21, m, m, m, m, m, m, m, m, m, m, m,
+		5, 6, 13, 14, 21, 22, m, m, m, m, m, m, m, m, m, m,
 		m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m,
 	}, res)
 	assert.Equal(t, uint64(5), zeroIndex)
 	assert.Equal(t, uint64(5), count)
 
 	res = resDef
-	_, count = Compare(1, &values[0], &res[0], 22)
+	_, count = Compare(1, &values[0], &res[0], 23)
 	assert.Equal(t, [32]uint64{
 		1, 4, 9, 12, 17, 20, m, m, m, m, m, m, m, m, m, m,
 		m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m,
@@ -30,7 +30,7 @@ func TestCompare(t *testing.T) {
 	assert.Equal(t, uint64(6), count)
 
 	res = resDef
-	_, count = Compare(2, &values[0], &res[0], 22)
+	_, count = Compare(2, &values[0], &res[0], 23)
 	assert.Equal(t, [32]uint64{
 		0, 2, 8, 10, 16, 18, m, m, m, m, m, m, m, m, m, m,
 		m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m,
@@ -38,7 +38,7 @@ func TestCompare(t *testing.T) {
 	assert.Equal(t, uint64(6), count)
 
 	res = resDef
-	_, count = Compare(3, &values[0], &res[0], 22)
+	_, count = Compare(3, &values[0], &res[0], 23)
 	assert.Equal(t, [32]uint64{
 		3, 11, 19, m, m, m, m, m, m, m, m, m, m, m, m, m,
 		m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m,
@@ -46,7 +46,7 @@ func TestCompare(t *testing.T) {
 	assert.Equal(t, uint64(3), count)
 
 	res = resDef
-	_, count = Compare(4, &values[0], &res[0], 22)
+	_, count = Compare(4, &values[0], &res[0], 23)
 	assert.Equal(t, [32]uint64{
 		7, 15, m, m, m, m, m, m, m, m, m, m, m, m, m, m,
 		m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m,
