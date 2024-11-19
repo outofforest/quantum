286cc4f0ccdcd73bf53acf5f5a6b02be29c6e368
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2024-11-19 11:28:47 +0100

    Store key hashes outside data items (#195)

--- space/data.go
+++ space/data.go
@@ -13,19 +13,24 @@ func NewDataNodeAssistant[K, V comparable]() (*DataNodeAssistant[K, V], error) {
 	itemSize := uint64(unsafe.Sizeof(types.DataItem[K, V]{})+types.UInt64Length-1) /
 		types.UInt64Length * types.UInt64Length
 
-	if itemSize > types.NodeLength {
-		return nil, errors.Errorf("item size %d is greater than node size %d", itemSize, types.NodeLength)
+	numOfItems := types.NodeLength / (itemSize + types.UInt64Length) // Uint64Length is for key hash.
+
+	if numOfItems == 0 {
+		return nil, errors.Errorf("item size %d is greater than node size %d",
+			(itemSize + types.UInt64Length), types.NodeLength)
 	}
 
 	return &DataNodeAssistant[K, V]{
 		itemSize:   itemSize,
-		numOfItems: types.NodeLength / itemSize,
+		itemOffset: numOfItems * types.UInt64Length, // Space reserved for key hashes.
+		numOfItems: numOfItems,
 	}, nil
 }
 
 // DataNodeAssistant converts nodes from bytes to data objects.
 type DataNodeAssistant[K, V comparable] struct {
 	itemSize   uint64
+	itemOffset uint64
 	numOfItems uint64
 }
 
@@ -36,7 +41,7 @@ func (na *DataNodeAssistant[K, V]) NumOfItems() uint64 {
 
 // ItemOffset returns item's offset relative to the beginning of the node.
 func (na *DataNodeAssistant[K, V]) ItemOffset(index uint64) uint64 {
-	return na.itemSize * index
+	return na.itemOffset + na.itemSize*index
 }
 
 // Item maps the memory address given by the node address and offset to an item.
@@ -44,14 +49,22 @@ func (na *DataNodeAssistant[K, V]) Item(n unsafe.Pointer, offset uint64) *types.
 	return (*types.DataItem[K, V])(unsafe.Add(n, offset))
 }
 
+// KeyHashes returns slice of key hashes stored in the node.
+func (na *DataNodeAssistant[K, V]) KeyHashes(n unsafe.Pointer) []types.KeyHash {
+	return unsafe.Slice((*types.KeyHash)(n), na.numOfItems)
+}
+
 // Iterator iterates over items.
-func (na *DataNodeAssistant[K, V]) Iterator(n unsafe.Pointer) func(func(*types.DataItem[K, V]) bool) {
-	return func(yield func(item *types.DataItem[K, V]) bool) {
-		for range na.numOfItems {
-			if !yield((*types.DataItem[K, V])(n)) {
+func (na *DataNodeAssistant[K, V]) Iterator(n unsafe.Pointer) func(func(uint64, *types.DataItem[K, V]) bool) {
+	return func(yield func(uint64, *types.DataItem[K, V]) bool) {
+		keyHashP := n
+		itemP := unsafe.Add(n, na.itemOffset)
+		for i := range na.numOfItems {
+			if *(*types.KeyHash)(keyHashP) != 0 && !yield(i, (*types.DataItem[K, V])(itemP)) {
 				return
 			}
-			n = unsafe.Add(n, na.itemSize)
+			keyHashP = unsafe.Add(keyHashP, types.UInt64Length)
+			itemP = unsafe.Add(itemP, na.itemSize)
 		}
 	}
 }
