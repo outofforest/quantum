a0579230bdf44995a86bd527c2fdf611c610aa3f
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-12-03 15:55:41 +0100

    Encode state and hash mod flag inside volatile address

--- types/types.go
+++ types/types.go
@@ -12,56 +12,11 @@ const (
 
 	// NodeLength is the number of bytes in the node.
 	NodeLength = 4096
-
-	// NumOfBlocks defines how many blocks to hash are present in the node.
-	NumOfBlocks = NodeLength / BlockLength
-)
-
-// State enumerates possible slot states.
-type State byte
-
-// State values are intentionally chosen in a way where following states differ by one bit only.
-// It is designed this way to guarantee that goroutines read (maybe outdated but) valid state values.
-// This hack is probably not needed because x86 CPUs guarantee write consistency on DWORD level,
-// but this business is crazy and nothing is going to surprise me.
-const (
-	// StateFree means slot is free.
-	StateFree State = 0
-
-	// StateData means slot contains data.
-	StateData State = 1
-
-	// StatePointer means slot contains pointer.
-	StatePointer State = 3
-)
-
-// Flags defines pointer flags.
-type Flags byte
-
-// IsSet checks if flag is set.
-func (f Flags) IsSet(flag Flags) bool {
-	return f&flag != FlagNone
-}
-
-// Set sets flag.
-func (f Flags) Set(flag Flags) Flags {
-	return f | flag
-}
-
-const (
-	// FlagNone is used to verify flags using bitwise and operator.
-	FlagNone Flags = 0
-
-	// FlagHashMod says that key hash must be recalculated.
-	FlagHashMod Flags = 1 << (iota - 1)
 )
 
 type (
 	// SnapshotID is the type for snapshot ID.
 	SnapshotID uint64
-
-	// NodeAddress represents the address of a node.
-	NodeAddress uint64
 
 	// KeyHash is the type for key hash.
 	KeyHash uint64
@@ -73,14 +28,70 @@ type (
 	SpaceID uint64
 )
 
+// NodeAddress represents the address of a node.
+type NodeAddress uint64
+
+// State returns node state.
+func (na NodeAddress) State() State {
+	switch {
+	case na == 0:
+		return StateFree
+	case na.IsSet(FlagPointerNode):
+		return StatePointer
+	default:
+		return StateData
+	}
+}
+
+// IsSet checks if flag is set.
+func (na NodeAddress) IsSet(flag NodeAddress) bool {
+	return na&flag != FreeAddress
+}
+
+// Set sets flag.
+func (na NodeAddress) Set(flag NodeAddress) NodeAddress {
+	return na | flag
+}
+
+// Naked returns address without flags.
+func (na NodeAddress) Naked() NodeAddress {
+	return na & flagNaked
+}
+
+const (
+	// FreeAddress means address is not assigned.
+	FreeAddress NodeAddress = 0
+
+	// flagNaked is used to retrieve address without flags.
+	flagNaked = FlagPointerNode - 1
+
+	// FlagPointerNode says that this is pointer node.
+	FlagPointerNode NodeAddress = 1 << 62
+
+	// FlagHashMod says that key hash must be recalculated.
+	FlagHashMod NodeAddress = 1 << 63
+)
+
+// State enumerates possible slot states.
+type State byte
+
+const (
+	// StateFree means slot is free.
+	StateFree State = iota
+
+	// StateData means slot contains data.
+	StateData
+
+	// StatePointer means slot contains pointer.
+	StatePointer
+)
+
 // Pointer is the pointer to another block.
 type Pointer struct {
 	SnapshotID        SnapshotID
 	VolatileAddress   NodeAddress
 	PersistentAddress NodeAddress
 	Revision          uint32
-	State             State
-	Flags             Flags
 }
 
 // NodeRoot represents the root of node.
