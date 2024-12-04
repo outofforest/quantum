81aff350505f7806f5b8a56d7350c8f968405727
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2024-12-04 19:23:03 +0100

    Test operations on conflicting key hashes (#273)

--- space/no_concurrency_test.go
+++ space/no_concurrency_test.go
@@ -195,3 +195,163 @@ func TestCRUDOnRootDataNode(t *testing.T) {
 	requireT.False(exists)
 	requireT.Equal(txtypes.Amount(0), balance)
 }
+
+// TestSetConflictingHashesOnRootDataNode sets many keys using the same hash to verify that they are not overwritten
+// by each other.
+func TestSetConflictingHashesOnRootDataNode(t *testing.T) {
+	const (
+		numOfItems                  = 50
+		snapshotID types.SnapshotID = 1
+		keyHash    types.KeyHash    = 1 // Same key hash is intentionally used for all the items to test conflicts.
+	)
+
+	requireT := require.New(t)
+
+	state, err := alloc.RunInTest(t, stateSize, nodesPerGroup)
+	requireT.NoError(err)
+
+	s := NewSpaceTest[txtypes.Account, txtypes.Amount](t, state, hashKey)
+
+	// Create
+
+	for i := range uint8(numOfItems) {
+		v, err := s.NewEntry(snapshotID, TestKey[txtypes.Account]{
+			Key:     txtypes.Account{i},
+			KeyHash: keyHash,
+		}, StagePointer0)
+		requireT.NoError(err)
+		requireT.NoError(s.SetKey(v, txtypes.Amount(i)))
+	}
+
+	// Verify items exist.
+
+	for i := range uint8(numOfItems) {
+		key := TestKey[txtypes.Account]{
+			Key:     txtypes.Account{i},
+			KeyHash: keyHash,
+		}
+		amount := txtypes.Amount(i)
+
+		v, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+
+		exists, err := s.KeyExists(v)
+		requireT.NoError(err)
+		requireT.True(exists)
+
+		balance, err := s.ReadKey(v)
+		requireT.NoError(err)
+		requireT.Equal(amount, balance)
+
+		balance, exists = s.Query(key)
+		requireT.True(exists)
+		requireT.Equal(amount, balance)
+	}
+
+	// Update every second item.
+
+	for i := uint8(0); i < numOfItems; i += 2 {
+		key := TestKey[txtypes.Account]{
+			Key:     txtypes.Account{i},
+			KeyHash: keyHash,
+		}
+		amount := txtypes.Amount(10 * i)
+
+		v, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+		requireT.NoError(s.SetKey(v, amount))
+
+		v2, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+
+		exists, err := s.KeyExists(v2)
+		requireT.NoError(err)
+		requireT.True(exists)
+
+		balance, err := s.ReadKey(v2)
+		requireT.NoError(err)
+		requireT.Equal(amount, balance)
+
+		balance, exists = s.Query(key)
+		requireT.True(exists)
+		requireT.Equal(amount, balance)
+	}
+
+	// Verify all the other items stay untouched.
+
+	for i := uint8(1); i < numOfItems; i += 2 {
+		key := TestKey[txtypes.Account]{
+			Key:     txtypes.Account{i},
+			KeyHash: keyHash,
+		}
+		amount := txtypes.Amount(i)
+
+		v, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+
+		exists, err := s.KeyExists(v)
+		requireT.NoError(err)
+		requireT.True(exists)
+
+		balance, err := s.ReadKey(v)
+		requireT.NoError(err)
+		requireT.Equal(amount, balance)
+
+		balance, exists = s.Query(key)
+		requireT.True(exists)
+		requireT.Equal(amount, balance)
+	}
+
+	// Delete every second item.
+
+	for i := uint8(0); i < numOfItems; i += 2 {
+		key := TestKey[txtypes.Account]{
+			Key:     txtypes.Account{i},
+			KeyHash: keyHash,
+		}
+
+		v, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+		requireT.NoError(s.DeleteKey(v))
+
+		v2, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+
+		exists, err := s.KeyExists(v2)
+		requireT.NoError(err)
+		requireT.False(exists)
+
+		balance, err := s.ReadKey(v2)
+		requireT.NoError(err)
+		requireT.Equal(txtypes.Amount(0), balance)
+
+		balance, exists = s.Query(key)
+		requireT.False(exists)
+		requireT.Equal(txtypes.Amount(0), balance)
+	}
+
+	// Verify all the other items still exist.
+
+	for i := uint8(1); i < numOfItems; i += 2 {
+		key := TestKey[txtypes.Account]{
+			Key:     txtypes.Account{i},
+			KeyHash: keyHash,
+		}
+		amount := txtypes.Amount(i)
+
+		v, err := s.NewEntry(snapshotID, key, StagePointer0)
+		requireT.NoError(err)
+
+		exists, err := s.KeyExists(v)
+		requireT.NoError(err)
+		requireT.True(exists)
+
+		balance, err := s.ReadKey(v)
+		requireT.NoError(err)
+		requireT.Equal(amount, balance)
+
+		balance, exists = s.Query(key)
+		requireT.True(exists)
+		requireT.Equal(amount, balance)
+	}
+}
