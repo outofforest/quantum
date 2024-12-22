5b98e1cce3ba239b96901c2cb758280e5362aa1f
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2024-12-22 20:55:25 +0100

    Test commit (#338)

--- db_test.go
+++ db_test.go
@@ -2,6 +2,7 @@ package quantum
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
@@ -194,6 +195,313 @@ func TestDelayedDeallocation(t *testing.T) {
 	deallocatedPersistentAddress3, err := persistentAllocator.Allocate()
 	requireT.NoError(err)
 	requireT.Equal(persistentAddress3, deallocatedPersistentAddress3)
+}
+
+func TestCommitWithoutDeallocationLists(t *testing.T) {
+	requireT := require.New(t)
+
+	const (
+		snapshotID1 types.SnapshotID = 1
+		snapshotID2 types.SnapshotID = 2
+	)
+
+	appState := state.NewForTest(t, 10*types.NodeLength)
+	txFactory := pipeline.NewTransactionRequestFactory()
+	volatileAllocator := appState.NewVolatileAllocator()
+	persistentAllocator := appState.NewPersistentAllocator()
+	persistentDeallocator := appState.NewPersistentDeallocator()
+	deallocationListsToCommit := map[types.SnapshotID]*types.ListRoot{}
+
+	snapshotInfoNodeAssistant, err := space.NewDataNodeAssistant[types.SnapshotID, types.SnapshotInfo]()
+	requireT.NoError(err)
+
+	var snapshotRoot, deallocationRoot types.Pointer
+	snapshotInfo1 := types.SnapshotInfo{
+		PreviousSnapshotID: 0,
+		NextSnapshotID:     snapshotID2,
+	}
+	snapshotInfo2 := types.SnapshotInfo{
+		PreviousSnapshotID: snapshotID1,
+		NextSnapshotID:     3,
+	}
+
+	snapshotSpace := space.New[types.SnapshotID, types.SnapshotInfo](space.Config[types.SnapshotID, types.SnapshotInfo]{
+		SpaceRoot: types.NodeRoot{
+			Pointer: &snapshotRoot,
+		},
+		State:             appState,
+		DataNodeAssistant: snapshotInfoNodeAssistant,
+		DeletionCounter:   lo.ToPtr[uint64](0),
+		NoSnapshots:       true,
+	})
+
+	deallocationNodeAssistant, err := space.NewDataNodeAssistant[deallocationKey, types.ListRoot]()
+	requireT.NoError(err)
+
+	deallocationListSpace := space.New[deallocationKey, types.ListRoot](
+		space.Config[deallocationKey, types.ListRoot]{
+			SpaceRoot: types.NodeRoot{
+				Pointer: &deallocationRoot,
+			},
+			State:             appState,
+			DataNodeAssistant: deallocationNodeAssistant,
+			DeletionCounter:   lo.ToPtr[uint64](0),
+			NoSnapshots:       true,
+		},
+	)
+
+	tx := txFactory.New()
+	requireT.NoError(commit(snapshotID1, snapshotInfo1, tx, appState, deallocationListsToCommit, volatileAllocator,
+		persistentAllocator, persistentDeallocator, snapshotSpace, deallocationListSpace))
+	requireT.Zero(deallocationRoot)
+	requireT.NotZero(snapshotRoot.VolatileAddress)
+	requireT.NotZero(snapshotRoot.PersistentAddress)
+	requireT.NotZero(snapshotRoot.SnapshotID)
+	requireT.NotZero(snapshotRoot.Revision)
+	sInfo1, exists := snapshotSpace.Query(snapshotID1)
+	requireT.True(exists)
+	requireT.Equal(snapshotInfo1, sInfo1)
+	requireT.Nil(tx.ListRequest)
+	requireT.Nil(*tx.LastListRequest)
+	requireT.Empty(deallocationListsToCommit)
+
+	// snapshot space
+
+	sr := tx.StoreRequest
+	requireT.NotNil(sr)
+	requireT.True(sr.NoSnapshots)
+	requireT.Equal(int8(1), sr.PointersToStore)
+	requireT.Nil(sr.Store[0].Hash)
+	requireT.NotZero(sr.Store[0].VolatileAddress)
+	requireT.NotZero(sr.Store[0].Pointer.VolatileAddress)
+	requireT.NotZero(sr.Store[0].Pointer.PersistentAddress)
+	requireT.Equal(snapshotRoot, *sr.Store[0].Pointer)
+	requireT.Equal(snapshotRoot.VolatileAddress, sr.Store[0].VolatileAddress)
+	requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+	requireT.Equal(snapshotID1, sr.Store[0].Pointer.SnapshotID)
+
+	// commit
+
+	sr = sr.Next
+	requireT.NotNil(sr)
+	requireT.Nil(sr.Next)
+	requireT.True(sr.NoSnapshots)
+	requireT.Equal(int8(1), sr.PointersToStore)
+	requireT.Nil(sr.Store[0].Hash)
+	requireT.Zero(sr.Store[0].VolatileAddress)
+	requireT.Zero(sr.Store[0].Pointer.VolatileAddress)
+	requireT.Equal(types.PersistentAddress(2), sr.Store[0].Pointer.PersistentAddress)
+	requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+	requireT.Equal(snapshotID1, sr.Store[0].Pointer.SnapshotID)
+
+	previousSnapshotRoot := snapshotRoot
+
+	tx = txFactory.New()
+	requireT.NoError(commit(snapshotID2, snapshotInfo2, tx, appState, deallocationListsToCommit, volatileAllocator,
+		persistentAllocator, persistentDeallocator, snapshotSpace, deallocationListSpace))
+	requireT.Zero(deallocationRoot)
+	requireT.NotZero(snapshotRoot.VolatileAddress)
+	requireT.NotZero(snapshotRoot.PersistentAddress)
+	requireT.NotZero(snapshotRoot.SnapshotID)
+	requireT.NotZero(snapshotRoot.Revision)
+	requireT.Equal(previousSnapshotRoot.VolatileAddress, snapshotRoot.VolatileAddress)
+	requireT.NotEqual(previousSnapshotRoot.PersistentAddress, snapshotRoot.PersistentAddress)
+	sInfo1, exists = snapshotSpace.Query(snapshotID1)
+	requireT.True(exists)
+	requireT.Equal(snapshotInfo1, sInfo1)
+	sInfo2, exists := snapshotSpace.Query(snapshotID2)
+	requireT.True(exists)
+	requireT.Equal(snapshotInfo2, sInfo2)
+	requireT.Nil(tx.ListRequest)
+	requireT.Nil(*tx.LastListRequest)
+	requireT.Empty(deallocationListsToCommit)
+
+	// snapshot space
+
+	sr = tx.StoreRequest
+	requireT.NotNil(sr)
+	requireT.True(sr.NoSnapshots)
+	requireT.Equal(int8(1), sr.PointersToStore)
+	requireT.Nil(sr.Store[0].Hash)
+	requireT.NotZero(sr.Store[0].VolatileAddress)
+	requireT.NotZero(sr.Store[0].Pointer.VolatileAddress)
+	requireT.NotZero(sr.Store[0].Pointer.PersistentAddress)
+	requireT.Equal(snapshotRoot, *sr.Store[0].Pointer)
+	requireT.Equal(snapshotRoot.VolatileAddress, sr.Store[0].VolatileAddress)
+	requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+	requireT.Equal(snapshotID2, sr.Store[0].Pointer.SnapshotID)
+
+	// commit
+
+	sr = sr.Next
+	requireT.NotNil(sr)
+	requireT.Nil(sr.Next)
+	requireT.True(sr.NoSnapshots)
+	requireT.Equal(int8(1), sr.PointersToStore)
+	requireT.Nil(sr.Store[0].Hash)
+	requireT.Zero(sr.Store[0].VolatileAddress)
+	requireT.Zero(sr.Store[0].Pointer.VolatileAddress)
+	requireT.Equal(types.PersistentAddress(4), sr.Store[0].Pointer.PersistentAddress)
+	requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+	requireT.Equal(snapshotID2, sr.Store[0].Pointer.SnapshotID)
+
+	persistentDeallocator.Deallocate(0x00)
+	appState.Commit()
+
+	var oldSnapshotRootFound bool
+	for {
+		persistentAddress, err := persistentAllocator.Allocate()
+		if err != nil {
+			break
+		}
+		requireT.NotZero(persistentAddress)
+		requireT.NotEqual(snapshotRoot.PersistentAddress, persistentAddress)
+		if persistentAddress == previousSnapshotRoot.PersistentAddress {
+			requireT.False(oldSnapshotRootFound)
+			oldSnapshotRootFound = true
+		}
+	}
+	requireT.True(oldSnapshotRootFound)
+}
+
+func TestCommitWithDeallocationLists(t *testing.T) {
+	requireT := require.New(t)
+
+	const (
+		snapshotID             types.SnapshotID = 1
+		numOfDeallocationLists                  = 5*pipeline.StoreCapacity - 1
+	)
+
+	appState := state.NewForTest(t, 10*types.NodeLength)
+	txFactory := pipeline.NewTransactionRequestFactory()
+	volatileAllocator := appState.NewVolatileAllocator()
+	persistentAllocator := appState.NewPersistentAllocator()
+	persistentDeallocator := appState.NewPersistentDeallocator()
+
+	snapshotInfoNodeAssistant, err := space.NewDataNodeAssistant[types.SnapshotID, types.SnapshotInfo]()
+	requireT.NoError(err)
+
+	var snapshotRoot, deallocationRoot types.Pointer
+	snapshotInfo := types.SnapshotInfo{
+		PreviousSnapshotID: 0,
+		NextSnapshotID:     2,
+	}
+
+	deallocationListsToCommit := map[types.SnapshotID]*types.ListRoot{}
+	for i := range types.SnapshotID(numOfDeallocationLists) {
+		deallocationListsToCommit[i] = &types.ListRoot{
+			VolatileAddress:   types.VolatileAddress(i),
+			PersistentAddress: types.PersistentAddress(i),
+		}
+	}
+
+	snapshotSpace := space.New[types.SnapshotID, types.SnapshotInfo](space.Config[types.SnapshotID, types.SnapshotInfo]{
+		SpaceRoot: types.NodeRoot{
+			Pointer: &snapshotRoot,
+		},
+		State:             appState,
+		DataNodeAssistant: snapshotInfoNodeAssistant,
+		DeletionCounter:   lo.ToPtr[uint64](0),
+		NoSnapshots:       true,
+	})
+
+	deallocationNodeAssistant, err := space.NewDataNodeAssistant[deallocationKey, types.ListRoot]()
+	requireT.NoError(err)
+
+	deallocationListSpace := space.New[deallocationKey, types.ListRoot](
+		space.Config[deallocationKey, types.ListRoot]{
+			SpaceRoot: types.NodeRoot{
+				Pointer: &deallocationRoot,
+			},
+			State:             appState,
+			DataNodeAssistant: deallocationNodeAssistant,
+			DeletionCounter:   lo.ToPtr[uint64](0),
+			NoSnapshots:       true,
+		},
+	)
+
+	tx := txFactory.New()
+	requireT.NoError(commit(snapshotID, snapshotInfo, tx, appState, deallocationListsToCommit, volatileAllocator,
+		persistentAllocator, persistentDeallocator, snapshotSpace, deallocationListSpace))
+	requireT.NotZero(deallocationRoot.VolatileAddress)
+	requireT.NotZero(deallocationRoot.PersistentAddress)
+	requireT.Equal(snapshotID, deallocationRoot.SnapshotID)
+	//	requireT.Equal(uintptr(unsafe.Pointer(tx.StoreRequest)), deallocationRoot.Revision)
+	requireT.NotNil(tx.ListRequest)
+	requireT.Nil(*tx.LastListRequest)
+	requireT.Empty(deallocationListsToCommit)
+
+	// deallocation space
+
+	sr := tx.StoreRequest
+	for i := range numOfDeallocationLists {
+		listRoot, exists := deallocationListSpace.Query(deallocationKey{
+			ListSnapshotID: snapshotID,
+			SnapshotID:     types.SnapshotID(i),
+		})
+		requireT.True(exists)
+		requireT.Equal(types.VolatileAddress(i), listRoot.VolatileAddress)
+		requireT.Equal(types.PersistentAddress(i), listRoot.PersistentAddress)
+
+		requireT.NotNil(sr)
+		requireT.True(sr.NoSnapshots)
+		requireT.Equal(int8(1), sr.PointersToStore)
+		requireT.Nil(sr.Store[0].Hash)
+		requireT.NotZero(sr.Store[0].VolatileAddress)
+		requireT.NotZero(sr.Store[0].Pointer.VolatileAddress)
+		requireT.NotZero(sr.Store[0].Pointer.PersistentAddress)
+		requireT.Equal(deallocationRoot, *sr.Store[0].Pointer)
+		requireT.Equal(deallocationRoot.VolatileAddress, sr.Store[0].VolatileAddress)
+		requireT.Equal(snapshotID, sr.Store[0].Pointer.SnapshotID)
+		if i == numOfDeallocationLists-1 {
+			requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+		}
+		sr = sr.Next
+	}
+
+	// snapshot space
+
+	requireT.NotNil(sr)
+	requireT.True(sr.NoSnapshots)
+	requireT.Equal(int8(1), sr.PointersToStore)
+	requireT.Nil(sr.Store[0].Hash)
+	requireT.NotZero(sr.Store[0].VolatileAddress)
+	requireT.NotZero(sr.Store[0].Pointer.VolatileAddress)
+	requireT.NotZero(sr.Store[0].Pointer.PersistentAddress)
+	requireT.Equal(snapshotRoot, *sr.Store[0].Pointer)
+	requireT.Equal(snapshotRoot.VolatileAddress, sr.Store[0].VolatileAddress)
+	requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+	requireT.Equal(snapshotID, sr.Store[0].Pointer.SnapshotID)
+
+	// commit
+
+	sr = sr.Next
+	requireT.NotNil(sr)
+	requireT.Nil(sr.Next)
+	requireT.True(sr.NoSnapshots)
+	requireT.Equal(int8(1), sr.PointersToStore)
+	requireT.Nil(sr.Store[0].Hash)
+	requireT.Zero(sr.Store[0].VolatileAddress)
+	requireT.Zero(sr.Store[0].Pointer.VolatileAddress)
+	requireT.Equal(types.PersistentAddress(2), sr.Store[0].Pointer.PersistentAddress)
+	requireT.Equal(uintptr(unsafe.Pointer(sr)), sr.Store[0].Pointer.Revision)
+	requireT.Equal(snapshotID, sr.Store[0].Pointer.SnapshotID)
+
+	var listRootIndex uint64
+	for lr := tx.ListRequest; lr != nil; lr = lr.Next {
+		if lr.Next == nil {
+			requireT.Equal(int8(pipeline.StoreCapacity-1), lr.ListsToStore)
+		} else {
+			requireT.Equal(int8(pipeline.StoreCapacity), lr.ListsToStore)
+		}
+		for i := range lr.ListsToStore {
+			listRoot := lr.List[i]
+			requireT.Equal(types.VolatileAddress(listRootIndex), listRoot.VolatileAddress)
+			requireT.Equal(types.PersistentAddress(listRootIndex), listRoot.PersistentAddress)
+			listRootIndex++
+		}
+	}
+	requireT.Equal(uint64(numOfDeallocationLists), listRootIndex)
 }
 
 func TestPipe01PrepareTransactionsDoesNothingIfTransactionIsNil(t *testing.T) {
